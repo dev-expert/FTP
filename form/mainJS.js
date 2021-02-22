65182a0c10bf4e0413c6adecfbe2e730a48aab33
himanshu.singh@appwrk.com <himanshu.singh@appwrk.com>
2021-02-22 09:43:03 +0530

    change

--- form/mainJS.js
+++ form/mainJS.js
@@ -1,4 +1,4 @@
-var person =  JSON.parse(localStorage.getItem("person"));
+var person =  JSON.parse(localStorage.getItem("person11"));
 //alert(person.name);
 document.getElementById("name").innerHTML = person.name;
 
@@ -8,7 +8,7 @@ document.getElementById("dis2").innerHTML = person.dis1;
 document.getElementById("iemail").innerHTML = person.email;
 document.getElementById("iphone").innerHTML = person.phone;
 document.getElementById("ild").innerHTML = person.link;
-document.getElementById("iadd").innerHTML = person.add;
+document.getElementById("iadd").innerHTML = person.Address;
 
 var skill = JSON.parse(localStorage.getItem("skills"));
 document.getElementById("skill1").innerHTML = skill.skill1;
@@ -21,12 +21,15 @@ document.getElementById("skill7").innerHTML = skill.skill7;
 document.getElementById("skill8").innerHTML = skill.skill8;
 document.getElementById("skill9").innerHTML = skill.skill9;
 
-document.getElementById("lan1").innerHTML = skill.lan1;
-document.getElementById("lan2").innerHTML = skill.lan2;
 
-document.getElementById("int1").innerHTML = skill.int1;
-document.getElementById("int2").innerHTML = skill.int2;
-document.getElementById("int3").innerHTML = skill.int3;
+var lang = JSON.parse(localStorage.getItem("lang"));
+document.getElementById("lan1").innerHTML = lang.lan1;
+document.getElementById("lan2").innerHTML = lang.lan2;
+
+var inter = JSON.parse(localStorage.getItem("inter"));
+document.getElementById("int1").innerHTML = inter.int1;
+document.getElementById("int2").innerHTML = inter.int2;
+document.getElementById("int3").innerHTML = inter.int3;
 
 var project = JSON.parse(localStorage.getItem("projects"));
 document.getElementById("pro11").innerHTML = project.pro11;
