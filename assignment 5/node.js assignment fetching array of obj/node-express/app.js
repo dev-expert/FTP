49a5529460197f245397d73b1786a6f2aa586b8a
5073akshay <akshay.sharma@appwrk.com>
2021-02-22 13:27:31 +0530

    created one more api name of list and listusers

--- assignment 5/node.js assignment fetching array of obj/node-express/app.js
+++ assignment 5/node.js assignment fetching array of obj/node-express/app.js
@@ -10,11 +10,17 @@ app.get('/listusers', function(req, res) {
         res.end(data);
     });
 })
+app.get('/list', function(req, res) {
+    fs.readFile('users.json', function(err, data) {
+        console.log(data);
+        res.end(data);
+    });
+})
 
 var server = app.listen(8081, function() {
     var host = server.address().address
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
-})
+});
 
 console.log('data written');
