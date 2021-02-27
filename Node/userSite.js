759eb95c587c172a2235bfa1d4e7db50ad1efe75
Himanshu Singh <him.h@appwrk.com>
2021-02-27 09:34:29 +0530

    nodejs

---
+++ Node/userSite.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://him:him@cluster1.nq5zu.mongodb.net/demoData?retryWrites=true&w=majority";
+
+var dbo;
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+
+    //var myobj = { name: "qwe", address: "abc" };
+    console.log("Database connected");
+    dbo = db.db("demoData");
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(8090, function (req, res) {
+    console.log("Running");
+})
+
+app.get("/", function (req, res) {
+    res.json({
+        message: 'Helloredtfgu World'
+    });
+})
+
+app.post("/signup", function (req, res) {
+    var q = req.body;
+    console.log("Post is working :" + q);
+
+
+    dbo.collection("user_data").insertOne(q, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+    })
+
+});
+
+
+
+app.get("/login", function (req, res) {
+    dbo.collection("user_data").find().toArray(function (err, result) {
+        if (err) throw err;
+
+        //console.log(result);
+
+        res.send("result");
+        res.json(result);
+    })
+})
+
+
+//     app.get("/admin", function (req, res) {
+
+// })
+
