c5700a0ddf8f08bf022f15c68884325264938e4c
anmol1221jha <78531461+anmol1221jha@users.noreply.github.com>
2021-03-01 10:03:28 +0530

    MongoDB with Form

---
+++ 27-02-2021 (MongoDB with Form)/login.js
@@ -0,0 +1,46 @@
+const Express = require("express");
+const Cors = require("cors");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const CONNECTION_URL = "mongodb+srv://Anik:123@cluster0.hbbhk.mongodb.net/Demo?retryWrites=true&w=majority";
+const DATABASE_NAME = "Demo";
+
+var app = Express();
+app.use(Cors());
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+var database, collection;
+
+//connection start
+app.listen(5000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if (error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("Login");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.get("/", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        console.log("Data Received");
+        response.send(result);
+    });
+});
+
+// export data
+app.post("/exportdata", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        console.log("Data Sent");
+        response.json(true);
+    });
+});
