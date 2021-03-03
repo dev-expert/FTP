fb278908300bae7f00df5a0024d0c376c63fa39b
Ashish <Ashish.chaudhary@appwork.com>
2021-03-03 18:15:34 +0530

    new code

--- cal/resume.js
+++ cal/resume.js
@@ -1,9 +1,11 @@
 function store() {
+    debugger;
    // var s = JSON.parse(localStorage.getItem("data"));
     var fname = document.getElementById("fname").value;
     var lname=document.getElementById("lname").value;
     var Dob=document.getElementById("Dob").value;
     var Martial=document.getElementById("Martial").value;
+    var Pic=document.getElementById("Pic").src;
 
     var Email =document.getElementById("Email").value;
     var Mobile=document.getElementById("Mobile").value;
@@ -13,7 +15,7 @@ function store() {
     var Year=document.getElementById("Year").value;
     var College =document.getElementById("College").value;
 
-    var Degr=document.getElementById("Degree").value;
+    var Degree=document.getElementById("Degree").value;
     var Univ=document.getElementById("Univ").value;
     var cgpa=document.getElementById("cgpa").value;
 
@@ -30,9 +32,17 @@ function store() {
     var Boad=document.getElementById("Boad").value;
     var cp=document.getElementById("cp").value;
 
+    var Project=document.getElementById("Project").value;
+    var Env=document.getElementById("Env").value;
+    var Lang=document.getElementById("Lang").value;
+    var Desc=document.getElementById("Desc").value;
+
+
+
+
     var Academic1=document.getElementById("Academic1").value;
     var Academic2=document.getElementById("Academic2").value;
-    var Academic36 =document.getElementById("Academic3").value;
+    var Academic3 =document.getElementById("Academic3").value;
     var Academic4=document.getElementById("Academic4").value;
 
     var achieviement1 =document.getElementById("achieviement1").value;
@@ -53,18 +63,70 @@ function store() {
     var language1 =document.getElementById("language1").value;
     var language2 =document.getElementById("language2").value;
 
-
+    var Martial =document.getElementById("Martial").value;
+    
     var data = {
-        fname: fname
+        fname: fname,
+        lname:lname,
+        Dob:Dob,
+        Martial:Martial,
+        Pic:Pic,
+        Email:Email,
+        Mobile:Mobile,
+        Place:Place,
+        Pic:Pic,
+        Year:Year,
+        College:College,
+        Degree:Degree,
+        Univ:Univ,
+        cgpa:cgpa,
+        Ye:Ye,
+        School:School,
+        Deg:Deg,
+        Board:Board,
+        cgp:cgp,
+        Yea:Yea,
+        sch:sch,
+        Degr:Degr,
+        Boad:Boad,
+        cp:cp,
+        Project:Project,
+        Env:Env,
+        Lang:Lang,
+        Desc:Desc,
+        Academic1:Academic1,
+        Academic2:Academic2,
+        Academic3:Academic3,
+        Academic4:Academic4,
+        achieviement1:achieviement1,
+        achieviement2:achieviement2,
+        achieviement3:achieviement3,
+        achieviement4:achieviement4,
+        achieviement5:achieviement5,
+        achieviement6:achieviement6,
+        Interpersonal1:Interpersonal1,
+        Interpersonal2:Interpersonal2,
+        Interpersonal3:Interpersonal3,
+        Interpersonal4:Interpersonal4,
+        hobby1:hobby1,
+        hobby2:hobby2,
+        language1:language1,
+        language2:language2,
+        Martial:Martial,
+
     }
     localStorage.setItem("data", JSON.stringify(data));
 }
 function showData() {
     debugger;
     var d = JSON.parse(localStorage.getItem("data"));
-    var firstname = d["fname"];
-    //var keys=Object.keys(fname);
-    //var s=keys[fname];
+    var s= Object.keys(d);
+    for(i=0;i<s.length;i++){
+        var f = s[i];
 
-    document.getElementById("fname").innerHTML = firstname;
+        switch(f){
+            case f:
+                document.getElementById(`${f}`).innerHTML= d[f];
+        }
+    }
 }
