4bd96905b80409c09cd643db1ddf58a632a409b3
Henry-Harpreet <henry.s@appwrk.com>
2021-04-21 19:22:15 +0530

    Socket and Hermes

---
+++ Hermes/withoutHermes/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function App() {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignSelf: 'center'}}>
+      <Text style={{ fontSize: 20 }}>Hello World Without Hermes</Text>
+    </View>
+  )
+}
