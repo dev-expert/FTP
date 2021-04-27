ce0ffe6c58136632330bdbf8a88ad0f281b58ec0
aron <aron.s@appwrk.com>
2021-04-27 13:51:38 +0530

    done modal in react js

--- React js/react_basics/src/App.js
+++ React js/react_basics/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import './App.css';
 //For importing the icon 
 // import { IconContext } from "react-icons"
@@ -8,20 +9,55 @@ import { FaReact } from "react-icons/fa";
 //Importing TOAST 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+//Importing modal 
+import Modal from 'react-modal';
+
+//styling for modal 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 function App() {
-  const notify = () => toast("Wow so easy!");
+
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
+  const notify = () => { toast("Wow so easy!", { position: toast.POSITION.TOP_LEFT }) };
   return (
     <div>
-      < IconContext.Provider value={{ color: 'yellow', size: '10REM' }}>
-        <div className="App">
-          <FaEllo />
-          <FaReact />
-        </div>
-      </IconContext.Provider >
+      <div>
+        {/* Icons */}
+        < IconContext.Provider value={{ color: 'yellow', size: '10REM' }}>
+          <div className="App">
+            <FaEllo />
+            <FaReact />
+          </div>
+        </IconContext.Provider >
+      </div>
+      <div>
+        {/* Toast notification */}
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
+      <div>
+        {/* Modal  */}
+        <button onClick={() => { setModalIsOpen(true) }} >Open modal </button>
+        <Modal isOpen={modalIsOpen} style={customStyles}>
+          <h2>Modal box</h2>
+          <p>yes</p>
+          {/* <div> */}
+          <button onClick={() => { setModalIsOpen(false) }} >Close  modal </button>
+          {/* </div> */}
+        </Modal>
+      </div>
+      <div>
 
-      <button onClick={notify}>Notify!</button>
-      <ToastContainer />
+      </div>
     </div>
   );
 }
