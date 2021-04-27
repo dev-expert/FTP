d5dc766935532621a4ee71d5779258a4105047e5
aron <aron.s@appwrk.com>
2021-04-27 13:11:15 +0530

    Toast done

--- React js/react_basics/src/App.js
+++ React js/react_basics/src/App.js
@@ -5,15 +5,24 @@ import './App.css';
 import { IconContext } from 'react-icons';
 import { FaEllo } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
+//Importing TOAST 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  const notify = () => toast("Wow so easy!");
   return (
-    < IconContext.Provider value={{ color: 'yellow', size: '10REM' }}>
-      <div className="App">
-        <FaEllo />
-        <FaReact />
-      </div>
-    </IconContext.Provider >
+    <div>
+      < IconContext.Provider value={{ color: 'yellow', size: '10REM' }}>
+        <div className="App">
+          <FaEllo />
+          <FaReact />
+        </div>
+      </IconContext.Provider >
+
+      <button onClick={notify}>Notify!</button>
+      <ToastContainer />
+    </div>
   );
 }
 
