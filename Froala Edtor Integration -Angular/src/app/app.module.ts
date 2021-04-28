f2ed70e6f7cb6dda53126646303f73862328e07f
Aric <aric.m@appwrk.com>
2021-04-28 18:47:14 +0530

    Integrated Froala Editor in Anglar.

---
+++ Froala Edtor Integration -Angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FroalaEditorModule.forRoot(),
+     FroalaViewModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
