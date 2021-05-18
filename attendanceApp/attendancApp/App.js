392e34ee6abb32c5ca7b5d27a4505cc5343d3d89
Henry-Harpreet <henry.s@appwrk.com>
2021-05-18 12:26:36 +0530

    Fix issue in Date coming from Database & keyboard cover input box

--- attendanceApp/attendancApp/App.js
+++ attendanceApp/attendancApp/App.js
@@ -14,6 +14,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Picker,
+  KeyboardAvoidingView,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -350,191 +351,197 @@ function CheckInOut({route, navigation}) {
   }
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}>
-      <View>
-        <LinearGradient
-          colors={['#833ab4', '#fd1d1d', '#fcb045']}
-          style={styles.myAttendanceBtn}>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('checkDetails', {
-                email: email,
-              });
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{flex: 1}}>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+        }}>
+        <View>
+          <LinearGradient
+            colors={['#833ab4', '#fd1d1d', '#fcb045']}
+            style={styles.myAttendanceBtn}>
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('checkDetails', {
+                  email: email,
+                });
+              }}>
+              <Text style={{fontSize: 20, color: 'white'}}>My Attendance</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-evenly',
+              marginTop: 100,
             }}>
-            <Text style={{fontSize: 20, color: 'white'}}>My Attendance</Text>
-          </TouchableOpacity>
-        </LinearGradient>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            marginTop: 100,
-          }}>
-          <TouchableOpacity
-            style={{
-              backgroundColor: checkInBtnColor,
-              width: 120,
-              height: 45,
-              alignItems: 'center',
-              paddingVertical: 7,
-            }}
-            onPress={() => {
-              checkInTTimeandgetLatLng(),
-                setflagCheckIn(true),
-                setflagCheckOut(false);
-            }}
-            disabled={flagCheckIn}>
-            <Text style={{fontSize: 20, color: 'white'}}>CheckIn</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: checkOutBtnColor,
-              width: 120,
-              height: 45,
-              alignItems: 'center',
-              paddingVertical: 7,
-            }}
-            onPress={() => {
-              checkOutTTimeDescRemoveLocalData(),
-                setflagCheckIn(false),
-                setflagCheckOut(true);
-            }}
-            disabled={flagCheckOut}>
-            <Text style={{fontSize: 20, color: 'white'}}>CheckOut</Text>
-          </TouchableOpacity>
-          {/* <TouchableOpacity
+            <TouchableOpacity
+              style={{
+                backgroundColor: checkInBtnColor,
+                width: 120,
+                height: 45,
+                alignItems: 'center',
+                paddingVertical: 7,
+              }}
+              onPress={() => {
+                checkInTTimeandgetLatLng(),
+                  setflagCheckIn(true),
+                  setflagCheckOut(false);
+              }}
+              disabled={flagCheckIn}>
+              <Text style={{fontSize: 20, color: 'white'}}>CheckIn</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: checkOutBtnColor,
+                width: 120,
+                height: 45,
+                alignItems: 'center',
+                paddingVertical: 7,
+              }}
+              onPress={() => {
+                checkOutTTimeDescRemoveLocalData(),
+                  setflagCheckIn(false),
+                  setflagCheckOut(true);
+              }}
+              disabled={flagCheckOut}>
+              <Text style={{fontSize: 20, color: 'white'}}>CheckOut</Text>
+            </TouchableOpacity>
+            {/* <TouchableOpacity
           onPress={() => {
             displaylocaldata();
           }}>
           <Text>Remove Item</Text>
         </TouchableOpacity> */}
-        </View>
-        <View style={{ opacity: breaktimeOpacity }}>
-          <Text style={{fontSize: 18, textAlign: 'center', marginTop: 30}}>
-            Break Time
-          </Text>
-          <View
-            style={{
-              borderBottomColor: 'black',
-              borderBottomWidth: 1,
-              width: '80%',
-              alignSelf: 'center',
-            }}
-          />
-          <View style={{flexDirection: 'row', alignSelf: 'center'}}>
-            <View>
-              <Text style={{fontSize: 18}}>Select Hrs</Text>
-              <Picker
-                selectedValue={selectedHourValue}
-                style={{height: 50, width: 150}}
-                onValueChange={(itemValue, itemIndex) =>
-                  setSelectedHourValue(itemValue)
-                }>
-                <Picker.Item label="01" value="01" />
-                <Picker.Item label="02" value="02" />
-                <Picker.Item label="03" value="03" />
-                <Picker.Item label="04" value="04" />
-                <Picker.Item label="05" value="05" />
-                <Picker.Item label="06" value="06" />
-                <Picker.Item label="07" value="07" />
-                <Picker.Item label="08" value="08" />
-              </Picker>
-            </View>
-            <View>
-              <Text style={{fontSize: 18}}>Select Mins</Text>
-              <Picker
-                selectedValue={selectedMinuteValue}
-                style={{height: 50, width: 150}}
-                onValueChange={(itemValue, itemIndex) =>
-                  setSelectedMinuteValue(itemValue)
-                }>
-                <Picker.Item label="01" value="01" />
-                <Picker.Item label="02" value="02" />
-                <Picker.Item label="03" value="03" />
-                <Picker.Item label="04" value="04" />
-                <Picker.Item label="05" value="05" />
-                <Picker.Item label="06" value="06" />
-                <Picker.Item label="07" value="07" />
-                <Picker.Item label="08" value="08" />
-                <Picker.Item label="09" value="09" />
-                <Picker.Item label="10" value="10" />
-                <Picker.Item label="11" value="11" />
-                <Picker.Item label="12" value="12" />
-                <Picker.Item label="13" value="13" />
-                <Picker.Item label="14" value="14" />
-                <Picker.Item label="15" value="15" />
-                <Picker.Item label="16" value="16" />
-                <Picker.Item label="17" value="17" />
-                <Picker.Item label="18" value="18" />
-                <Picker.Item label="19" value="19" />
-                <Picker.Item label="20" value="20" />
-                <Picker.Item label="21" value="21" />
-                <Picker.Item label="22" value="22" />
-                <Picker.Item label="23" value="23" />
-                <Picker.Item label="24" value="24" />
-                <Picker.Item label="25" value="25" />
-                <Picker.Item label="26" value="26" />
-                <Picker.Item label="27" value="27" />
-                <Picker.Item label="28" value="28" />
-                <Picker.Item label="29" value="29" />
-                <Picker.Item label="30" value="30" />
-                <Picker.Item label="31" value="31" />
-                <Picker.Item label="32" value="32" />
-                <Picker.Item label="33" value="33" />
-                <Picker.Item label="34" value="34" />
-                <Picker.Item label="35" value="35" />
-                <Picker.Item label="36" value="36" />
-                <Picker.Item label="37" value="37" />
-                <Picker.Item label="38" value="38" />
-                <Picker.Item label="39" value="39" />
-                <Picker.Item label="40" value="40" />
-                <Picker.Item label="41" value="41" />
-                <Picker.Item label="42" value="42" />
-                <Picker.Item label="43" value="43" />
-                <Picker.Item label="44" value="44" />
-                <Picker.Item label="45" value="45" />
-                <Picker.Item label="46" value="46" />
-                <Picker.Item label="47" value="47" />
-                <Picker.Item label="48" value="48" />
-                <Picker.Item label="49" value="49" />
-                <Picker.Item label="50" value="50" />
-                <Picker.Item label="51" value="51" />
-                <Picker.Item label="52" value="52" />
-                <Picker.Item label="53" value="53" />
-                <Picker.Item label="54" value="54" />
-                <Picker.Item label="55" value="55" />
-                <Picker.Item label="56" value="56" />
-                <Picker.Item label="57" value="57" />
-                <Picker.Item label="58" value="58" />
-                <Picker.Item label="59" value="59" />
-                <Picker.Item label="60" value="60" />
-              </Picker>
+          </View>
+          <View style={{opacity: breaktimeOpacity}}>
+            <Text style={{fontSize: 18, textAlign: 'center', marginTop: 30}}>
+              Break Time
+            </Text>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+                width: '80%',
+                alignSelf: 'center',
+              }}
+            />
+            <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+              <View>
+                <Text style={{fontSize: 18}}>Select Hrs</Text>
+                <Picker
+                  selectedValue={selectedHourValue}
+                  style={{height: 50, width: 150}}
+                  onValueChange={(itemValue, itemIndex) =>
+                    setSelectedHourValue(itemValue)
+                  }>
+                  <Picker.Item label="00" value="00" />
+                  <Picker.Item label="01" value="01" />
+                  <Picker.Item label="02" value="02" />
+                  <Picker.Item label="03" value="03" />
+                  <Picker.Item label="04" value="04" />
+                  <Picker.Item label="05" value="05" />
+                  <Picker.Item label="06" value="06" />
+                  <Picker.Item label="07" value="07" />
+                  <Picker.Item label="08" value="08" />
+                </Picker>
+              </View>
+              <View>
+                <Text style={{fontSize: 18}}>Select Mins</Text>
+                <Picker
+                  selectedValue={selectedMinuteValue}
+                  style={{height: 50, width: 150}}
+                  onValueChange={(itemValue, itemIndex) =>
+                    setSelectedMinuteValue(itemValue)
+                  }>
+                  <Picker.Item label="00" value="00" />
+                  <Picker.Item label="01" value="01" />
+                  <Picker.Item label="02" value="02" />
+                  <Picker.Item label="03" value="03" />
+                  <Picker.Item label="04" value="04" />
+                  <Picker.Item label="05" value="05" />
+                  <Picker.Item label="06" value="06" />
+                  <Picker.Item label="07" value="07" />
+                  <Picker.Item label="08" value="08" />
+                  <Picker.Item label="09" value="09" />
+                  <Picker.Item label="10" value="10" />
+                  <Picker.Item label="11" value="11" />
+                  <Picker.Item label="12" value="12" />
+                  <Picker.Item label="13" value="13" />
+                  <Picker.Item label="14" value="14" />
+                  <Picker.Item label="15" value="15" />
+                  <Picker.Item label="16" value="16" />
+                  <Picker.Item label="17" value="17" />
+                  <Picker.Item label="18" value="18" />
+                  <Picker.Item label="19" value="19" />
+                  <Picker.Item label="20" value="20" />
+                  <Picker.Item label="21" value="21" />
+                  <Picker.Item label="22" value="22" />
+                  <Picker.Item label="23" value="23" />
+                  <Picker.Item label="24" value="24" />
+                  <Picker.Item label="25" value="25" />
+                  <Picker.Item label="26" value="26" />
+                  <Picker.Item label="27" value="27" />
+                  <Picker.Item label="28" value="28" />
+                  <Picker.Item label="29" value="29" />
+                  <Picker.Item label="30" value="30" />
+                  <Picker.Item label="31" value="31" />
+                  <Picker.Item label="32" value="32" />
+                  <Picker.Item label="33" value="33" />
+                  <Picker.Item label="34" value="34" />
+                  <Picker.Item label="35" value="35" />
+                  <Picker.Item label="36" value="36" />
+                  <Picker.Item label="37" value="37" />
+                  <Picker.Item label="38" value="38" />
+                  <Picker.Item label="39" value="39" />
+                  <Picker.Item label="40" value="40" />
+                  <Picker.Item label="41" value="41" />
+                  <Picker.Item label="42" value="42" />
+                  <Picker.Item label="43" value="43" />
+                  <Picker.Item label="44" value="44" />
+                  <Picker.Item label="45" value="45" />
+                  <Picker.Item label="46" value="46" />
+                  <Picker.Item label="47" value="47" />
+                  <Picker.Item label="48" value="48" />
+                  <Picker.Item label="49" value="49" />
+                  <Picker.Item label="50" value="50" />
+                  <Picker.Item label="51" value="51" />
+                  <Picker.Item label="52" value="52" />
+                  <Picker.Item label="53" value="53" />
+                  <Picker.Item label="54" value="54" />
+                  <Picker.Item label="55" value="55" />
+                  <Picker.Item label="56" value="56" />
+                  <Picker.Item label="57" value="57" />
+                  <Picker.Item label="58" value="58" />
+                  <Picker.Item label="59" value="59" />
+                  <Picker.Item label="60" value="60" />
+                </Picker>
+              </View>
             </View>
           </View>
-        </View>
-        <View style={{marginTop: '10%', opacity: descriptionOpacity}}>
-          <Text style={{textAlign: 'center', fontSize: 20}}>Description</Text>
+          <View style={{marginTop: 10, opacity: descriptionOpacity}}>
+            <Text style={{textAlign: 'center', fontSize: 20}}>Description</Text>
 
-          <TextInput
-            editable={descriptionDisableFlag}
-            style={{
-              borderColor: 'black',
-              borderWidth: 1,
-              width: '98%',
-              alignSelf: 'center',
-              borderRadius: 5,
-            }}
-            multiline={true}
-            numberOfLines={4}
-            onChangeText={setDescription}
-            value={description}
-          />
+            <TextInput
+              editable={descriptionDisableFlag}
+              style={{
+                borderColor: 'black',
+                borderWidth: 1,
+                width: '98%',
+                alignSelf: 'center',
+                borderRadius: 5,
+              }}
+              multiline={true}
+              numberOfLines={4}
+              onChangeText={setDescription}
+              value={description}
+            />
+          </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -563,28 +570,56 @@ function checkDetails({route, navigation}) {
       payload,
     );
     let data = res.data;
-    // console.log('>>>>>>>>>>>>>', data);
+    console.log('>>>>>>>>>>>>>', data);
     let len = data.length;
     console.log('length', len);
     for (let i = 0; i < len; i++) {
       let data = res.data[i];
       // console.log('>>>>>>>>>>>>>', data);
+      let timestamp = new Date(data.checkInDate).getTime();
+      let obj = new Date(timestamp);
+      let month = obj.getMonth() + 1;
+      let year = obj.getFullYear();
+      let date = obj.getDate();
+      // console.log(timestamp)
+      if (month.toString().length < 2) {
+        month = '0' + month;
+      }
+      let checkInDate = year + '/' + month + '/' + date;
+      let checkinDateForColor = year + '-' + month + '-' + date;
+
+      let timestampcheckOut = new Date(data.checkOutDate).getTime();
+      let obj2 = new Date(timestampcheckOut);
+      let monthh = obj2.getMonth() + 1;
+      let yearr = obj2.getFullYear();
+      let datee = obj2.getDate();
+      // console.log(timestamp)
+      if (monthh.toString().length < 2) {
+        monthh = '0' + monthh;
+      }
+      let checkOutDate = yearr + '/' + monthh + '/' + datee;
+      let checkOutDateForColor = year + '/' + month + '/' + date;
 
       const checkInTIME = data.checkInTime.slice(0, 8);
       const checkOutTIME = data.checkOutTime.slice(0, 8);
-      const checkInDATE = data.checkindateandtime.slice(0, 10);
-      const checkOutDATE = data.checkindateandtime.slice(0, 10);
+      const checkInDATE = checkInDate;
+      const checkOutDATE = checkOutDate;
+      console.log(
+        'date:--------------------------------------------------------------',
+        checkInDATE,
+      );
+      // const replacedCheckInDATE = checkInDATE.replace(/-/g, '/');
+      // const replacedCheckOUTDATE = checkOutDATE.replace(/-/g, '/');
 
-      const replacedCheckInDATE = checkInDATE.replace(/-/g, '/');
-      const replacedCheckOUTDATE = checkOutDATE.replace(/-/g, '/');
-
-      const combineInDateAndTime = `${replacedCheckInDATE} ${checkInTIME}`;
-      const combineOutDateAndTime = `${replacedCheckOUTDATE} ${checkOutTIME}`;
+      const combineInDateAndTime = `${checkInDATE} ${checkInTIME}`;
+      const combineOutDateAndTime = `${checkOutDATE} ${checkOutTIME}`;
 
       const start = new Date(`${combineInDateAndTime}`).getTime();
       const end = new Date(`${combineOutDateAndTime}`).getTime();
+      console.log(start + '--------------------' + end);
       const difference = end - start;
       console.log('difference----', difference);
+      // difference = difference+data.breakTime
 
       // const diffminusbreak = difference - data.breakTime;
 
@@ -602,22 +637,23 @@ function checkDetails({route, navigation}) {
       settime(time => [...time, totaltime]);
 
       if (difference >= 34200000) {
-        setgreendate(greendate => [...greendate, checkInDATE]);
+        setgreendate(greendate => [...greendate, checkinDateForColor]);
         setColor('green');
       } else if (difference < 34200000) {
-        setOrangedate(orangedate => [...orangedate, checkInDATE]);
+        setOrangedate(orangedate => [...orangedate, checkinDateForColor]);
         setColor('orange');
       }
 
-      setgreenorange(greenorange => [...greenorange, checkInDATE]);
+      setgreenorange(greenorange => [...greenorange, checkinDateForColor]);
 
       const milisecondss = data.breakTime;
       const minu = Math.floor((milisecondss / 1000 / 60) % 60);
       const hou = Math.floor((milisecondss / 1000 / 3600) % 24);
-      if (minu.toString.length < 2) {
+      if (minu.toString().length < 2) {
         minu = '0' + minu;
       }
-      if (hou.toString.length < 2) {
+
+      if (hou.toString().length < 2) {
         hou = '0' + hou;
       }
       console.log('-----------', hou, ':', minu);
